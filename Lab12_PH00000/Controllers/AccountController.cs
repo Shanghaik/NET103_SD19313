0f1ce5353f1ded6cdbde19d6e0b3aa29a781a32b
shanghaik168@gmail.com <Shanghaik1998>
2024-09-14 10:48:00 +0700

    Add project files.

---
+++ Lab12_PH00000/Controllers/AccountController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Lab12_PH00000.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login(string username, string password)
+        {
+            // trường hợp mới mở form, không có gì cả
+            if (string.IsNullOrEmpty(username) && string.IsNullOrEmpty(password)) return View();
+            else
+            {
+                // Nếu username = admin và password không rỗng => Chuyển hướng về trang chủ
+                if (username.ToLower() == "admin" && password.Trim().Length > 0)
+                {
+                    TempData["message"] = "Bạn đã đăng nhập với tài khoản admin";
+                    return RedirectToAction("Index", "Hame"); // chuyển hướng về trang chủ
+                }
+                else
+                {
+                    TempData["message"] = "Bạn đã đăng nhập với tài khoản thường";
+                    return RedirectToAction("Privacy", "Hame"); // còn lại về trang privacy
+                }
+            }
+            /*
+             * TempData được sử dụng để lưu trữ tạm dữ liệu cho đến khi dữ liệu đó được đọc hoặc có 1
+             * request khác được đưa ra, vòng đời của dữ liệu nằm trong tempdata là giữa 2 request
+             * Tempdata được sử dungjt heo cơ chế key - value
+             * TempData đó thể được đọc trực tiếp trong action hoặc trong View với cú pháp @TempData[key]
+             */
+        }
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+        // Thực hiện điều hướng với RedirectToAction - điều hướng tới action
+        public IActionResult GoHome()
+        {
+            // return RedirectToAction("Register"); // Khi chỉ điền tên action nó sẽ hiểu là action đó nằm trong cùng controller
+            // return RedirectToAction("Index", "Hame"); //=> Chạy OK
+            return Redirect("/Hame/Index"); // điều hướng trực tiếp //=> Chạy OK
+        }
+    }
+}
---
+++ Lab12_PH00000/Controllers/HameController.cs
@@ -0,0 +1,40 @@
+﻿using Lab12_PH00000.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Lab12_PH00000.Controllers
+{
+    public class HameController : Controller
+    {
+        private readonly ILogger<HameController> _logger;
+
+        public HameController(ILogger<HameController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            // ViewBag/Viewdata được sử dụng để truyền dữ liệu từ controller sang View nhưng phạm vi sử dụng
+            // chỉ nằm trong View ứng với Action
+            // Vòng đời của Viewbag/Viewdata cũng chỉ nằm trong 1 Request duy nhất
+            // ViewBag là dạng dữ liệu dynamic Object
+            // ViewData se giống tempData theo cơ chế key-value
+            // tương tự như tempdata, ta cũng có thể gọi dữ liệu trực tiếp từ ViewBag Viewdata với @
+            ViewBag.message = TempData["message"];
+            ViewData["data"] = TempData["message"];
+            return View(); // Trả về / hiển thị ra 1 View có cùng tên với Action
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
